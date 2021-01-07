5d19b79c7a0635e43fcd26cfbec22b48c4f8bc58
Fiona Henry <fionahenry87@googlemail.com>
2021-01-07 00:47:14 +0000

    Updated WhyUCP page

---
+++ src/pages/WhyUCP.js
@@ -0,0 +1,29 @@
+export default function WhyUCP() {
+    return (
+        <>
+        <br></br>
+        <div class="tile is-ancestor">
+            <div class="tile is-vertical is-8">
+                <br></br>
+                <article class="tile is-child notification is-info">
+                    <p class="title">WERDSSSSS</p>
+                    <br></br>
+
+                </article>
+            </div>
+            
+            <div class="tile is-parent">
+                    <article class="tile is-child notification is-primary">
+                        <div class="content is-centered">
+                            <p class="subtitle">Graduate at Peterborough Cathedral</p>
+                        <figure class="image">
+                            <img src="./Grad2.jpg" alt=""></img>
+                        </figure>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        <br></br>
+        </>
+    );
+};
