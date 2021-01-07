4059493268bd924cc4278ab2a2f164c0e2652b1f
Fiona Henry <fionahenry87@googlemail.com>
2021-01-07 00:58:52 +0000

    Added to Apply page

---
+++ src/pages/Apply.js
@@ -0,0 +1,12 @@
+export default function Apply() {
+    return (
+        <>
+        <br></br>
+        <div class="tile is-vertical">
+            <article class="tile is-child notification is-info">
+                <p>Applying to UCP is easy!</p>
+            </article>
+        </div>
+        </>
+    );
+};
