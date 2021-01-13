808c5490656b62256f76dd99f89b42db34c11c76
Fiona Henry <fionahenry87@googlemail.com>
2021-01-13 21:02:04 +0000

    Added more content to Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,15 @@ export default function Home() {
             <div class="tile is-vertical is-8">
                 <div class="tile">
                 <div class="tile is-parent is-vertical">
-                    <article class="tile is-child notification">
-                        <div id="wheelOfFortune">
-                            <canvas id="wheel" width="700" height="700"></canvas>
-                            <div id="spin">SPIN</div>
-                            <script  src="./SpinWheel.js"></script>
-                        </div>
+                    <article class="tile is-child notification is-primary">
+                        <figure class="image is-4by2">
+                            <img src="./January.png" alt=""></img>
+                        </figure>
                     </article>
                 </div>
                 <div class="tile is-parent">
                     <article class="tile is-child notification is-primary">
+                        <br></br>
                     <figure class="image is-4by2">
                         <img src="./Virtual.jpg" alt=""></img>
                     </figure>
@@ -23,11 +22,14 @@ export default function Home() {
                 </div>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child notification">
-                    <p class="title">Wide tile</p>
-                    <p class="subtitle">Aligned with the right tile</p>
+                <article class="tile is-child notification is-primary">
+                    <p class="title">  Welcome to UCP</p>
                     <div class="content">
-                    <p></p>
+                        <p>While you are studying with us we aim to support your development and assist you in achieving your career ambitions. </p>
+                        <p>We very much care about you as individuals and one of our key objectives is to ensure we listen to your views and continuously develop our provision so that your needs are met.</p>
+                        <p>The University Centre offers a broad portfolio of courses covering a range of subject areas and we are rapidly developing specialisms in a number of key areas.</p>
+                        <p>University Centre Peterborough (UCP) was established in 2007 and was a joint venture between Anglia Ruskin University (ARU) and Peterborough College. We have now become an approved partner of ARU and a wholly owned subsidiary of the Inspire Education Group from 1st August 2020. ARU accredits our undergraduate degree courses at the Peterborough campus. Higher National provision accredited by Pearson is delivered at both the Peterborough and Stamford campuses. UCP at Stamford also has a range of teaching and education courses accredited by Bishop Grosseteste University.</p>
+                        <p>Employability has been another area we have developed at UCP. We have forged excellent working relationships with companies such as, BGL, Saffery Champness as well as numerous third sector organisations such as the Citizen’s’ Advice Bureau. Companies recruit direct from us and also offer students paid internships and commissioned projects. We also have excellent connections with volunteer organisations and support you in your personal development from year one. As a successful partner in the national OfS Challenge Fund we have extended the work of our Employability Hub to engage with SMEs in the region.</p>
                     </div>
                 </article>
                 </div>
@@ -35,8 +37,8 @@ export default function Home() {
             <div class="tile is-parent">
                 <article class="tile is-child notification is-primary">
                 <div class="content">
-                    <p class="title">Tall tile</p>
-                    <p class="subtitle">With even more content</p>
+                    <br></br>
+                    <br></br>
                     <div class="content">
                     <figure class="image">
                         <img src="./Students.png" alt=""></img>
