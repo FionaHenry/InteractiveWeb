4876e0b0bec687e80315001031effb54b05f9162
Fiona Henry <fionahenry87@googlemail.com>
2021-01-13 21:24:19 +0000

    Added content to whyucp page

--- src/pages/WhyUCP.js
+++ src/pages/WhyUCP.js
@@ -5,17 +5,33 @@ export default function WhyUCP() {
         <div class="tile is-ancestor">
             <div class="tile is-vertical is-8">
                 <br></br>
-                <article class="tile is-child notification is-info">
-                    <p class="title">WERDSSSSS</p>
+                <article class="tile is-child notification is-success">
+                    <p class="title">Why Study at UCP?</p>
+                    <p>At UCP you will learn from lecturers who are highly qualified and experienced in their fields. But great knowledge isn’t the only thing our staff can offer; they are also friendly, supportive and dedicated to helping you achieve your ambitions.</p>
+                    <p>Our campuses are located in the heart of Peterborough and Stamford, and just a short walk into the city or town centres.</p>
                     <br></br>
-
+                    <p class="title">Study at our modern campuses</p>
+                    <p>For the majority of our undergraduate courses, all lectures and seminars will take place at the Peterborough or Stamford campuses, which are open Monday to Friday throughout the year.</p>
+                    <p>Our modern campuses include the following features:</p>
+                    <div class="content">
+                    <ul>
+                        <li>96 seater lecture theatre</li>
+                        <li>Fully equipped classrooms</li>
+                        <li>Costa or Starbucks café</li>
+                        <li>Large social atrium or common room</li>
+                        <li>Computer / digital suites</li>
+                        <li>Quiet study zones</li>
+                        <li>Open plan / group study areas</li>
+                        <li>Student Support Centre</li>
+                    </ul>
+                    </div>
                 </article>
             </div>
             
             <div class="tile is-parent">
                     <article class="tile is-child notification is-primary">
                         <div class="content is-centered">
-                            <p class="subtitle">Graduate at Peterborough Cathedral</p>
+                            <p class="title">Graduate at Peterborough Cathedral</p>
                         <figure class="image">
                             <img src="./Grad2.jpg" alt=""></img>
                         </figure>
