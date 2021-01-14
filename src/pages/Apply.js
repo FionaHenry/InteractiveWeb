09a13231f01081c3f042250867101d8caf46af87
Fiona Henry <fionahenry87@googlemail.com>
2021-01-14 00:05:18 +0000

    Added content to Apply page

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -2,10 +2,22 @@ export default function Apply() {
     return (
         <>
         <br></br>
-        <div class="tile is-vertical">
-            <article class="tile is-child notification is-info">
-                <p>Applying to UCP is easy!</p>
-            </article>
+        <div class="tile is-ancestor">
+            <div class="tile is-parent">
+                <p class="title">Applying to UCP is easy!</p>
+            </div>
+        </div>
+        <div class="tile is-ancestor">
+            <div class="tile is-parent is-4">
+                <p>If you are applying for a full-time undergraduate bachelor’s course you will need to make an online application through UCAS. Applications to UCAS are made online using their web-based Tracker System via ‘Apply’</p>
+                <br></br>
+            </div>
+            <div class="tile is-child is-2">
+                <a href="https://www.ucas.com/undergraduate/applying-to-university" target="_blank">UCAS Apply</a>
+            </div>
+            <div class="tile is-child">
+                <p>Applications begin 1st September and the 1st deadline is 15 January in the year before the programme commences, although it is possible to apply later than this if courses still have vacancies. Anyone applying after the 30 June by 18:00 will be automatically entered into ‘Clearing’. Clearing takes place after the publication of the A Level results in August when universities will have a clearer picture of how many offers have been accepted and therefore how many vacancies they may have.</p>
+            </div>
         </div>
         </>
     );
